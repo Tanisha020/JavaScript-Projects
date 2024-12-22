4d4c09e56d8aa22f94c5eea892dadbae0f5c7ecb
Tanisha020 <tanisha2318@gmail.com>
2024-12-22 15:49:13 +0530

    added BMI calculator with HTML, CSS, and JavaScript

---
+++ 02_BMI_calculator/script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+
+    const results = document.querySelector('#results')
+    if(height == "" || isNaN(height) || height < 0){
+        results.innerHTML = 'Please enter valid height.'
+    }else if(weight == "" || isNaN(weight) || weight < 0){
+        results.innerHTML = 'Please enter valid weight.'
+    }else{
+        const bmi = (weight / ((height * height)/10000)).toFixed(2)
+        results.innerHTML = `<span>BMI: ${bmi}</span>`
+    }
+})
